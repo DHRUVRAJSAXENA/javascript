c131037946e14d5e90f5e49335199ead323cdcb8
DHRUVRAJSAXENA <dhruvrajsaxena1234@gmail.com>
2024-02-23 14:02:57 +0530

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,14 @@
+// types of datatypes in javascript
+
+/*
+ number = 134
+ string
+ bigint
+ boolean = true/false
+ null
+ undefined
+ symbol => unique
+*/
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
